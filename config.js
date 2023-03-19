df5d5728f5ecb67cd2a4f607e79a6cd6dd7b3f64
Mrezor <mrezorfr@hotmail.com>
2023-03-19 13:28:40 +0100

    BOT_PREFIX_V14

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clients: {
+        token: '',
+        prefix: '+',
+        activity: 'Dev By Mrezor ❤️',
+        embedColor: 'Random',
+        name: 'Mrezor ❤️',
+        logo: 'https://yt3.ggpht.com/2HFQ6HfIATwmh90bEhFXqu88k7tZTp29nZ4DihFVflITUaWqjqSu-TtkxWFliFdTE70kJUotjg=s900-c-k-c0x00ffffff-no-rj'
+    }
+}
